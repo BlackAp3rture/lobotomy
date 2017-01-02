a8b1e201cbf82c632d9bac4867751976794eebfb
rotlogix <rotlogix@gmail.com>
2017-01-02 15:49:02 -0500

    Added command auto-complete to the surgical module

--- core/cli/surgical.py
+++ core/cli/surgical.py
@@ -1,5 +1,6 @@
 from cmd2 import Cmd as SurgicalCmd
 from core.logging.logger import Logger
+from functools import wraps
 from blessings import Terminal
 from datetime import datetime
 from core.brains.utilities.util import Util
@@ -11,6 +12,10 @@ from core.brains.surgical.modules.crypto import CryptoModule
 from pygments import highlight
 from pygments.lexers import JavaLexer
 from pygments.formatters import TerminalFormatter
+import readline
+# DO NOT REMOVE
+# Fix for cmd2 that enables command auto-complete
+readline.parse_and_bind("bind ^I rl_complete")
 
 
 class SurgicalError(Exception):
@@ -18,6 +23,35 @@ class SurgicalError(Exception):
         self.logger = Logger()
         self.message = message
         self.logger.log("critical", "Surgical : {}".format(self.message))
+
+
+class SurgicalCmdArgumentException(Exception):
+    def __init__(self, cmdargs=None, doc=""):
+        self.t = Terminal()
+        if not cmdargs:
+            cmdargs = None
+        self.cmdargs = cmdargs
+        self.doc = doc
+
+    def __str__(self):
+        msg = [self.doc]
+        if self.cmdargs:
+            msg.insert(0, "\n\t{0} : {1} (!)".format("Command not found",
+                                                     self.cmdargs))
+        return "\n\n".join(msg)
+
+
+def cmd_arguments(expected_args):
+    def decorator(func):
+        func._expected_args = expected_args
+        @wraps(func)
+        def wrapper(self, *args):
+            if args[0].split(" ")[0] not in expected_args:
+                raise SurgicalCmdArgumentException(cmdargs=args[0].split(" ")[0],
+                                                   doc=func.func_doc)
+            return func(self, *args)
+        return wrapper
+    return decorator
 
 
 class Run(SurgicalCmd):
@@ -42,37 +76,57 @@ class Run(SurgicalCmd):
         self.target_module = None
         self.methods_api_usage = list()
 
-    def do_modules(self, args):
+    def _cmd_completer(self, name, text, line, begidx, endidx):
+        fn = getattr(self, 'do_'+name)
+        if not hasattr(fn.im_func, "_expected_args"):
+            return []
+        a = [arg for arg in fn.im_func._expected_args if arg.startswith(text)]
+        return a
+
+    def complete_modules(self, *args):
+        return self._cmd_completer("modules", *args)
+
+    @cmd_arguments(["list", "select"])
+    def do_modules(self, *args):
         """
         List and select target API modules.
 
         := modules list
         := modules select
         """
-
         # Locals
+        arg0 = args[0]
         selection = None
 
         try:
-            if args.split()[0] == "list":
+            if arg0 == "list":
                 if self.modules:
                     print("\n")
-                    for m in self.modules:
-                        if m:
-                            print(self.t.cyan("\t--> {}".format(m.name)))
+                    for i, m in enumerate(self.modules):
+                        print(self.t.cyan("\t--> [{}] {}"
+                                          .format(i, m.name)))
                     print("\n")
-            if args.split()[0] == "select":
+            if arg0 == "select":
                 if self.modules:
                     selection = raw_input(self.t.yellow("[{}] ".format(datetime.now())) + "Select module : ")
-                    for m in self.modules:
-                        if m:
-                            if selection == m.name:
-                                self.target_module = m
-                                self.logger.surgical_log("info", "{} module selected (!)".format(m.name))
+                    try:
+                        index = int(selection)
+                    except ValueError:
+                        index = -1
+                    for i, m in enumerate(self.modules):
+                        if selection == m.name or i == index:
+                            self.target_module = m
+                            break
+                    self.logger.surgical_log("info", "{} module selected (!)"
+                                             .format(self.target_module.name))
         except Exception as e:
             SurgicalError(e.message)
 
-    def do_api(self, args):
+    def complete_api(self, *args):
+        return self._cmd_completer("api", *args)
+
+    @cmd_arguments(["list", "select", "analyzed"])
+    def do_api(self, *args):
         """
         List and select methods from a given loaded API module
 
@@ -81,15 +135,16 @@ class Run(SurgicalCmd):
         := api analyzed list
         := api analyzed select
         """
-
         # Locals
+        arg0 = args[0].split(" ")[0]
+        arg1 = None
         class_selection = None
         method_selection = None
         surgical_lib = None
 
         try:
             # List the available API methods from the target module
-            if args.split()[0] == "list":
+            if arg0 == "list":
                 if self.target_module:
                     print("\n")
                     for k, v in self.target_module.model.values.items():
@@ -102,13 +157,11 @@ class Run(SurgicalCmd):
                 else:
                     self.logger.surgical_log("info", "Target module has not been loaded (!)")
             # Select an API method from the target module
-            elif args.split()[0] == "select":
+            elif arg0 == "select":
                 if self.target_module:
                     # TODO Consider building a wrapper around raw_input()
-                    class_selection = raw_input(self.t.yellow("[{}] "
-                                                              .format(datetime.now())) + "Select class : ")
-                    method_selection = raw_input(self.t.yellow("[{}] "
-                                                               .format(datetime.now())) + "Select method : ")
+                    class_selection = raw_input(self.t.yellow("[{}] ".format(datetime.now())) + "Select class : ")
+                    method_selection = raw_input(self.t.yellow("[{}] ".format(datetime.now())) + "Select method : ")
                     for k, v in self.target_module.model.values.items():
                         # This is so we can support classes with identical
                         # method names --> Ex: java.util.zip.ZipFile
@@ -130,38 +183,38 @@ class Run(SurgicalCmd):
                                     # tuples
                                     self.methods_api_usage = surgical_lib.search()
                                 else:
-                                    self.logger.surgical_log("warn",
-                                                             "Method not found (!)")
+                                    self.logger.surgical_log("warn","Method not found (!)")
             # Analyze the processed method list
-            elif args.split()[0] == "analyzed":
+            elif arg0 == "analyzed":
+                if args[0].split(" ")[1]:
+                    arg1 = args[0].split(" ")[1]
                 # List the methods that have been processed
-                if args.split()[1] == "list":
+                if arg1 == "list":
                     if self.methods_api_usage:
                         print("\n")
-                        for m in self.methods_api_usage:
-                            print(self.t.cyan("\t--> {} -> {} "
-                                              .format(m[0].class_name,
+                        for i, m in enumerate(self.methods_api_usage):
+                            print(self.t.cyan("\t--> [{}] {} -> {} "
+                                              .format(i, m[0].class_name,
                                                       m[0].name)))
                         print("\n")
                     else:
                         SurgicalError("API usage not found (!)")
                 # Select from the processed method list
-                elif args.split()[1] == "select":
+                elif arg1 == "select":
                     if self.methods_api_usage:
-                        selection = raw_input(self.t.yellow("[{}] "
-                                                            .format(datetime.now())) + "Select method : ")
-                        for m in self.methods_api_usage:
-                            if selection == m[0].name:
+                        selection = raw_input(self.t.yellow("[{}] ".format(datetime.now())) + "Select method : ")
+                        try:
+                            index = int(selection)
+                        except ValueError:
+                            index = -1
+                        for i, m in enumerate(self.methods_api_usage):
+                            if selection == m[0].name or i == index:
                                 print("\n")
-                                print(self.t.cyan("\t--> Class : {}"
-                                                  .format(m[0].class_name)))
-                                print(self.t.cyan("\t\t--> Method : {}"
-                                                  .format(m[0].name)))
+                                print(self.t.cyan("\t--> Class : {}".format(m[0].class_name)))
+                                print(self.t.cyan("\t\t--> Method : {}".format(m[0].name)))
                                 print(self.t.cyan("\t\t\t --> XREFS ###########"))
-                                self.u.print_xref("T",
-                                                  m[1].method.XREFto.items)
-                                self.u.print_xref("F",
-                                                  m[1].method.XREFfrom.items)
+                                self.u.print_xref("T", m[1].method.XREFto.items)
+                                self.u.print_xref("F", m[1].method.XREFfrom.items)
                                 print("\n")
                                 print(highlight(m[2],
                                                 JavaLexer(),
